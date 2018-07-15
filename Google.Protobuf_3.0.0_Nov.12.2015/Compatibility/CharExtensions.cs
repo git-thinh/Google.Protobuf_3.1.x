f0e27d9a8baf15ddcfaa7e3f11fd94da268f6067
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-07-15 23:47:46 +0700

    a

---
+++ Google.Protobuf_3.0.0_Nov.12.2015/Compatibility/CharExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Google.Protobuf.Compatibility
+{
+    internal static class CharExtensions
+    {
+        internal static bool IsHighSurrogate(this char target)
+        {
+            return target >= '\uD800' && target<= '\uDBFF';
+        }
+        internal static bool IsLowSurrogate(this char target)
+        {
+            return target >= '\uDC00' && target <= '\uDFFF';
+        }
+    }
+}
